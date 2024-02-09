c2f07f7ab75208ac10fc827088d2b89e4d990dc6
Eduardo <educosta.costa@gmail.com>
2024-02-09 22:26:26 +0000

    admin

--- contact/admin.py
+++ contact/admin.py
@@ -6,5 +6,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id','first_name','last_name','phone',
+    ordering = 'id',
+    #list_filter = 'created_date',
+    search_fields = 'id','first_name',
+    list_per_page = 10
+    list_max_show_all = 300
+    #list_editable = 'first_name'
 
