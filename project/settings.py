b82c93bc6b08454c9f54ba5ca424b6a79bf6f6a7
Eduardo <educosta.costa@gmail.com>
2024-02-10 23:22:00 +0000

    create contacts

--- project/settings.py
+++ project/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-pt'
+LANGUAGE_CODE = 'pt-PT'
 
 TIME_ZONE = 'Europe/Lisbon'
 
