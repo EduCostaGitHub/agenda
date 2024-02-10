01b8f8fca63e69de2a37ece2925c29462b5933d5
Eduardo <educosta.costa@gmail.com>
2024-02-10 22:30:03 +0000

    owner

--- contact/admin.py
+++ contact/admin.py
@@ -13,4 +13,11 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_max_show_all = 300
     #list_editable = 'first_name'
+    list_display_links = 'first_name',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = 'id',
+  
 
