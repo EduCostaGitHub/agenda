05512b296e1c039517c95a32e6a523bd9ddfe2e4
Eduardo <educosta.costa@gmail.com>
2024-02-13 18:09:36 +0000

    pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,17 +1,54 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from contact.models import Contact
-from django.http import Http404
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[:10]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts' : contacts,
+        'page_obj' : page_obj,
         'site_title': 'Contacts'
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+def search(request):
+    search_value = request.GET.get('q','').strip()
+
+    if search_value == '':
+        return redirect('contact:index')  
+    
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value),
+        )\
+        .order_by('-id')
+
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)      
+
+    context = {
+        'page_obj' : page_obj,
+        'site_title': 'search'
     }
 
     return render(
