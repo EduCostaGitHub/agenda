92e35e4062d5a9ea8d8163cb0c9ff01b6d16ec18
Eduardo <educosta.costa@gmail.com>
2024-02-19 17:35:56 +0000

    fomrs

--- contact/forms.py
+++ contact/forms.py
@@ -1,9 +1,36 @@
 from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
 from contact.models import Contact
-from typing import Any
+from typing import Any, Mapping
 from django.core.exceptions import ValidationError
 
 class ContactForm(forms.ModelForm):
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             'class': 'classse-A',
+    #             'placeholder':'Write your first name'
+    #         }
+    #     )
+    # )
+    first_name = forms.CharField(
+        help_text='help Text',
+        widget=forms.TextInput(
+            attrs={                
+                'placeholder':'Write your first name'
+            }
+        ),
+        label='First Name',
+    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['phone'].widget.attrs.update({
+            'placeholder':'your phone number'
+        })
+
     class Meta:
         model = Contact
         fields = (
@@ -11,8 +38,12 @@ class ContactForm(forms.ModelForm):
             'last_name',
             'phone',
         )
-        widgets = {
-            'first_name': forms.PasswordInput()
+        widgets = {            
+            'last_name': forms.TextInput(
+                attrs={
+                    'placeholder': "Write your last name"
+                }
+            )
         }
 
     def clean(self) -> dict[str, Any]:
